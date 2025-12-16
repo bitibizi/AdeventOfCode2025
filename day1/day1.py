b6d1ef4e7acd1d792eb54cd3c98fc21e8857506b
bitibizi <hanbit199@gmail.com>
2025-12-16 14:25:50 +0100

    finish day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -12,21 +12,36 @@ def part1(lst):
         if point == 0:
             password += 1
         number = int(item[1:])
-        result = number % 100
         if item[0] == "L":
-            diff = point - result
+            diff = point - number
             point = diff % 100
         else:
-            add = point + result
+            add = point + number
             point = add % 100
 
     return password  
-            
-                
-                
-    
 
+def part2(lst):
+    point = 50
+    password = 0
+    for item in lst:
+        number = int(item[1:])
+        if item[0] == "L":
+            diff = point - number
+            if diff <= 0:
+                if point != 0:
+                    password += 1
+                password += int(-diff/100)
+            point = diff % 100
+        else:
+            add = point + number
+            point = add % 100
+            if add >= 100:
+                password += int(add/100)
+    return password
+            
 
 if __name__ == "__main__":
     lines = get_input('input.txt')
     print("First star:", part1(lines))
+    print("Second star:", part2(lines))
