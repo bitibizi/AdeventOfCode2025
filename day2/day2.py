6bbab6c6d4eea53b7452e00ec30e2084ddee63d2
bitibizi <hanbit199@gmail.com>
2025-12-19 01:57:06 +0100

    finish day 2 part2

--- day2/day2.py
+++ day2/day2.py
@@ -19,9 +19,33 @@ def part1(lst):
             if first_part == second_part:
                 sum_num += number
     return sum_num
-            
-    
+
+def part2(lst):
+    sum_num = 0
+    for item in lst:
+        x = range(item[0], item[-1] + 1)
+        for number in x:
+            found_pattern = False
+            str_num = str(number)
+            length = len(str_num)
+            max_size_pattern = int(len(str_num)/2)
+            for i in range(1, max_size_pattern+1):
+                if length % i == 0:
+                    current_pattern_ok = True
+                    for j in range(0, length):
+                        if str_num[j] != str_num[j % i]:
+                            current_pattern_ok = False
+                            break
+                    if current_pattern_ok:
+                        found_pattern = True
+                        break
+            if found_pattern:
+                sum_num += number
+
+    return sum_num
+
+
 if __name__ == "__main__":
     lines = get_input('input.txt')
     print("First star:", part1(lines))
-    # print("Second star:", part2(lines))
+    print("Second star:", part2(lines))
